639929475133a307675a4a4aed57f83f7f6a15d6
Garret DePass <garretdepass@gmail.com>
2025-05-09 12:08:39 -0400

    added Wind to character sheet and a function to calculate it from Spirit and Vigor. Removed unnecessary "type: trait" key value pair from a couple lines in characterTemplate.json

--- src/components/character_view.js
+++ src/components/character_view.js
@@ -1,15 +1,62 @@
+// import { forEach } from "cypress/types/lodash";
 import React from "react";
 import { Link } from 'react-router-dom';
 
 const CharacterView = ({character}) => {
 
-return (
-    <div>
-        <div>{character.name}</div>
-        <div>Pace: {character.stats.pace}</div>
-        <Link to="/">Change Character</Link>
-    </div>
-)
+
+const returnStats = (stat, trait) => {
+    return (
+        <div>
+            {stat.name} {stat.dieCount}d{trait.dieSides}
+        </div>
+    )
+}
+
+const returnTotalWind = () => {
+    const spiritIndex = character.stats.traits.findIndex((object) => object.name === "Spirit");
+    const vigorIndex = character.stats.traits.findIndex((object) => object.name === "Vigor");
+    console.log(`the index of spirit is ${spiritIndex}`);
+    return (
+        character.stats.traits[spiritIndex].dieSides + character.stats.traits[vigorIndex].dieSides
+    )
+}
+
+    return (
+        <div>
+            <div>{character.name}</div>
+            <div>Grit: {character.stats.grit}</div>
+            <div>Pace: {character.stats.pace}</div>
+            <div>Size: {character.stats.size}</div>
+            <div>Wind: {returnTotalWind()} </div>
+            <br></br>
+            {character.stats.traits.map(
+                trait =>  
+                <div>
+                    {returnStats(trait, trait)}
+
+                    {Array.isArray(trait.attributes) && trait.attributes.map(
+                        attribute => <div>
+                            {returnStats(attribute, trait)}
+                        
+                        {Array.isArray(attribute.concentrations) && attribute.concentrations.map(
+                            concentration => <div>
+                                {returnStats(concentration, trait)}
+                            </div>
+                        )
+                        }
+                        </div>
+                    )
+                    }               
+                        
+                    <br></br>
+                </div>
+            )
+            }                
+            <Link to="/">Change Character</Link>
+        </div>
+            )
+
 
 }
 
